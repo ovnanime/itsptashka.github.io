8b5f5939df882ab88b07ac616c96214352183fed
ovnanime <itsptashka@gmail.com>
2026-06-02 19:21:33 +0300

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ptahen.ru',        // твой кастомный домен
+  base: '/',                        // можно оставить /, т.к. это username.github.io
+  trailingSlash: 'never',
+});
