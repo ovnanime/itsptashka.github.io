a15d571bb63de5ee19c69a4679a48e40db25a433
ovnanime <itsptashka@gmail.com>
2026-07-24 21:08:17 +0300

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  devToolbar: { enabled: false },
+  site: 'https://ptahen.ru',
+  base: '/',
+  trailingSlash: 'never',
+  integrations: [mdx()],
+});
