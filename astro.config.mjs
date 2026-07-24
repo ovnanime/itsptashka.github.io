78d0f7f7480fe2cc80a82971fa4555f6bac66982
ovnanime <itsptashka@gmail.com>
2026-07-24 20:52:54 +0300

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://ptahen.ru',        // твой кастомный домен
-  base: '/',                        // можно оставить /, т.к. это username.github.io
-  trailingSlash: 'never',
-});
